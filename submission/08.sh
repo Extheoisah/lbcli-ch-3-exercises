0859f70bf9e564f83a8cac0e2e4efd8f885d1cc6
Kelvin Isievwore <kelvinator4leo@gmail.com>
2025-02-26 23:22:08 +0100

    Add Exercises

---
+++ submission/08.sh
@@ -0,0 +1,4 @@
+# (true / false) Verify the signature by this address over this message:
+#   address: `musT1kFhLBhBYS2x7VseJWGvEXtTKQUMvm`
+#   message: `btrust-builders`
+#   signature: `H4xvHLiCvfuZZVWyHYYqVkwuf9TJHkUFFbB0lmOZFU0rKFOE+a6Zjkv7qm8qWn9ImftiDaEuWhzikYJoC4wqvhg=`
---
+++ submission/09.sh
@@ -0,0 +1,2 @@
+# Using descriptors, compute the taproot address at index 100 derived from this extended public key:
+#   `tpubDDtwnJf2Um8U6dZ2yuYiHYeufFb6cUuQzwUB7vGpd9K9LvVe7LUuBXWS29GTeRs4j6MafvAAHNKkfqjq93LYMNPwhRgVgWUSmfVEktsdwpX`
