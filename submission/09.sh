4bb7ab7bde13b3d29fbcf7b5c08253f7f2fa1ed5
Theophilus <isahtheophilus@gmail.com>
2025-02-28 00:59:50 +0100

    feat: pass stage 9

--- submission/09.sh
+++ submission/09.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `tpubDDtwnJf2Um8U6dZ2yuYiHYeufFb6cUuQzwUB7vGpd9K9LvVe7LUuBXWS29GTeRs4j6MafvAAHNKkfqjq93LYMNPwhRgVgWUSmfVEktsdwpX`
+
+DESCRIPTOR=$(bitcoin-cli -regtest -rpcwallet=btrustwallet -named getdescriptorinfo descriptor="tr(tpubDDtwnJf2Um8U6dZ2yuYiHYeufFb6cUuQzwUB7vGpd9K9LvVe7LUuBXWS29GTeRs4j6MafvAAHNKkfqjq93LYMNPwhRgVgWUSmfVEktsdwpX/100)" | grep -o '"descriptor": "[^"]*"' | cut -d'"' -f4)
+
+bitcoin-cli -regtest -named deriveaddresses descriptor="$DESCRIPTOR" | grep -o '"bcrt1[^"]*"' | tr -d '"'
