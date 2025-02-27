9cc71e159380d9333629b37bf91d545e5429f3c5
Theophilus <isahtheophilus@gmail.com>
2025-02-28 00:11:24 +0100

    feat: pass stage 7

--- submission/08.sh
+++ submission/08.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
 # (true / false) Verify the signature by this address over this message:
 #   address: `musT1kFhLBhBYS2x7VseJWGvEXtTKQUMvm`
 #   message: `btrust-builders`
 #   signature: `H4xvHLiCvfuZZVWyHYYqVkwuf9TJHkUFFbB0lmOZFU0rKFOE+a6Zjkv7qm8qWn9ImftiDaEuWhzikYJoC4wqvhg=`
+
+echo 'H4xvHLiCvfuZZVWyHYYqVkwuf9TJHkUFFbB0lmOZFU0rKFOE+a6Zjkv7qm8qWn9ImftiDaEuWhzikYJoC4wqvhg=' > /tmp/sig.txt
+bitcoin-cli -regtest -rpcwallet=btrustwallet verifymessage musT1kFhLBhBYS2x7VseJWGvEXtTKQUMvm "btrust-builders" "$(cat /tmp/sig.txt)"
+rm /tmp/sig.txt
