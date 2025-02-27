9dffd8c15b6aa8236e6b4ac70a643fdd90aabd1b
Theophilus <isahtheophilus@gmail.com>
2025-02-27 20:06:24 +0100

    chore: test again

--- submission/01.sh
+++ submission/01.sh
@@ -1,8 +1,8 @@
 # Create a wallet with the name "btrustwallet".
-$ bitcoin-cli -regtest createwallet "btrustwallet"
+# $ bitcoin-cli -regtest createwallet "btrustwallet"
 
-# # Get the block count.
-# $ bitcoin-cli -regtest getblockcount
+# Get the block count.
+$ bitcoin-cli -regtest getblockcount
 
-# # Get the block hash of the block at height 100.
+# Get the block hash of the block at height 100.
 # $ bitcoin-cli -regtest getblockhash 100
