121af205c9139b31e6a48bea35acd978371e9084
Theophilus <isahtheophilus@gmail.com>
2025-02-27 20:09:56 +0100

    chore: make test pass

--- submission/01.sh
+++ submission/01.sh
@@ -1,8 +1,4 @@
+#!/bin/bash
+
 # Create a wallet with the name "btrustwallet".
-# $ bitcoin-cli -regtest createwallet "btrustwallet"
-
-# Get the block count.
-$ bitcoin-cli -regtest getblockcount
-
-# Get the block hash of the block at height 100.
-# $ bitcoin-cli -regtest getblockhash 100
+bitcoin-cli -regtest createwallet "btrustwallet"
